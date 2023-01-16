7f8ea0e527abc8409fa3d4c7dc46d8dbe4dc5c52
SpenceDog <git@spencerlynch.us>
2023-01-15 22:21:17 -0500

    Set up base files

---
+++ README.md
@@ -0,0 +1,2 @@
+<h1>The Odin Project Calculator</h2>
+<p>This is a simple calculator project for The Odin Project!</p>
---
+++ calc.js
@@ -0,0 +1,8 @@
+function Operate(){ // This is going to do our calculations for us.
+    alert("a");
+}
+
+function writeNum(){ // adds the specified field to the text box.
+    var text = document.getElementById('lower-number');
+    text.value += ' after clicking';
+}
